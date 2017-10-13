588d810f79672321023ccf988294c9499534cb7d
Dominic Fajilan <dfajilan@csumb.edu>
2017-10-13 19:26:51 +0000

    Homework 3

--- index.php
+++ index.php
@@ -32,17 +32,17 @@
                 <option value="no">No</option>
             </select><br><br>
             Question 1: What is Zelda's race?<br>
-            <input type="radio" name="q1" value="goron">Goron<br>
+            <input type="radio" name="q1" value="goron" required>Goron<br>
             <input type="radio" name="q1" value="zora">Zora<br>
             <input type="radio" name="q1" value="hylian">Hylian<br>
             <input type="radio" name="q1" value="rito">Rito<br><br>
             Question 2: How long was Link asleep?<br>
-            <input type="radio" name="q2" value="1">1 Year<br>
+            <input type="radio" name="q2" value="1" required>1 Year<br>
             <input type="radio" name="q2" value="100">100 Years<br>
             <input type="radio" name="q2" value="25">25 Years<br>
             <input type="radio" name="q2" value="143">143 Years<br><br>
             Question 3: What is the name of setting for The Legend of Zelda?<br>
-            <input type="radio" name="q3" value="hyrule">Hyrule<br>
+            <input type="radio" name="q3" value="hyrule" required>Hyrule<br>
             <input type="radio" name="q3" value="kanto">Kanto<br>
             <input type="radio" name="q3" value="mushroom">Mushroom Kingdom<br>
             <input type="radio" name="q3" value="final">Final Destination<br><br>
