ce325197c568cc9e61e84dee7190d15a81129755
Dominic Fajilan <dfajilan@csumb.edu>
2017-10-13 19:09:09 +0000

    Homework 3

--- result.php
+++ result.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
     <head>
         <title>Legend of Zelda Quiz</title>
@@ -8,11 +7,11 @@
         </style>
     </head>
     <body>
-         <h1>The Legend of Zelda Breath of the Wild<br>
+         <h1>The Legend of Zelda: Breath of the Wild<br>
             Quiz!</h1>
             
         <?php
-            echo "Thank you <u>" . $_POST["name"] . "</u> for participating in this questionnaire";
+            echo "<div id='thankyou'>Thank you <u>" . $_POST["name"] . "</u> for participating in this questionnaire";
             
             if ($_POST["own"] == "yes")
             {
@@ -21,7 +20,7 @@
                 if ($_POST["rating"] == "yes")
                     echo "<br> We're glad you enjoyed your time in the land of Hyrule!";
                 else
-                    echo "<br> We're sorry that you did not enjoy your stay in the land of Hyrule.";
+                    echo "<br> We're sorry that you did not enjoy your stay in the land of Hyrule.</div>";
             }
             else 
                 echo "!";
@@ -32,9 +31,14 @@
                 $num_correct++;
             if ($_POST["q2"] == "100")
                 $num_correct++;
+            if ($_POST["q3"] == "Hyrule")
                 $num_correct++;
             
-            echo "<br><br> Your score: " . $num_correct . "/3";
+            echo "<br><br> <div id='return'>Your score: " . $num_correct . "/3<br><br>";
         ?>
+            <form action="index.php">
+                <input type="submit" value="Return">
+            </form>
+        </div>
     </body>
 </html>
